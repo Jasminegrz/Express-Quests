740285b8a9b0f2ed964cc236f56c03a24223a000
Jasmine Grozinger <jasmine.grozinger@gmail.com>
2023-05-17 09:45:50 +0200

    add database users

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const app = express();
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my list of users");
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
