80c627c4d521ab978b5cd63ce23d79589fad207f
Jasmine Grozinger <jasmine.grozinger@gmail.com>
2023-05-18 13:07:22 +0200

    corrected route get

--- userHandlers.js
+++ userHandlers.js
@@ -16,7 +16,7 @@ const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from movies where id = ?", [id])
+    .query("select * from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
         res.json(users[0]);
